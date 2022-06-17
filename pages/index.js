3017fa38f6eeb260ba99ff2ff312cc387c0bea57
petrkucerak <petr@khome.cz>
2022-06-17 14:20:02 +0200

    Install tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-export default function Index(){
-  return(
-    <h1>Hello world!</h1>
-  )
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl bg-sky-700">Hello world!</h1>
+    </main>
+  );
 }
