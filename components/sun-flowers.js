3ca6d14db639e5ce62e83fb1a7ba2023e0948f1b
petrkucerak <petr@khome.cz>
2022-06-20 18:07:11 +0200

    Add alt descritpion

--- components/sun-flowers.js
+++ components/sun-flowers.js
@@ -1,9 +1,21 @@
 export default function SunFlowers() {
   return (
     <div className="">
-      <img src="/assets/image/cover-left-top.png" className="h-96 absolute top-0 left-0"></img>
-      <img src="/assets/image/cover-left-button.png" className="h-96 absolute bottom-0 left-0"></img>
-      <img src="/assets/image/cover-right.png" className="h-96 absolute top-0 right-0"></img>
+      <img
+        src="/assets/image/cover-left-top.png"
+        className="h-96 absolute top-0 left-0"
+        alt="sunflower"
+      ></img>
+      <img
+        src="/assets/image/cover-left-button.png"
+        className="h-96 absolute bottom-0 left-0"
+        alt="sunflower"
+      ></img>
+      <img
+        src="/assets/image/cover-right.png"
+        className="h-96 absolute top-0 right-0"
+        alt="sunflower"
+      ></img>
     </div>
   );
 }
