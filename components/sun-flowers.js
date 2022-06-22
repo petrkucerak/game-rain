0ca8f691e1f04fcb6695ee83191d2ab66338e546
petrkucerak <petr@khome.cz>
2022-06-22 11:46:53 +0200

    responsive sunflowers

--- components/sun-flowers.js
+++ components/sun-flowers.js
@@ -3,17 +3,17 @@ export default function SunFlowers() {
     <div className="">
       <img
         src="/assets/image/cover-left-top.png"
-        className="h-96 absolute top-0 left-0"
+        className="md:h-96 h-48 absolute top-0 left-0"
         alt="sunflower"
       ></img>
       <img
         src="/assets/image/cover-left-button.png"
-        className="h-96 absolute bottom-[10vh] left-0"
+        className="md:h-96 h-48 absolute bottom-[10vh] left-0"
         alt="sunflower"
       ></img>
       <img
         src="/assets/image/cover-right.png"
-        className="h-96 absolute top-0 right-0"
+        className="md:h-96 h-48 absolute top-0 right-0"
         alt="sunflower"
       ></img>
     </div>
