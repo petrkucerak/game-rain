4fca328f7b78a698118191194e8269547161c78c
petrkucerak <petr@khome.cz>
2022-06-22 14:26:21 +0200

    Chnage button color program

--- components/program.js
+++ components/program.js
@@ -7,25 +7,25 @@ export default function Program() {
         <h2 className="text-6xl mb-6">Harmonogram</h2>
         <ul className="text-2xl ">
           <li className="border-l-4 pl-4 border-stone-800">
-            <div className="h-5 w-5 bg-stone-400 rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
+            <div className="h-5 w-5 bg-sunflower rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
             <storng className="font-semibold mr-4">12:30</storng> Obřad v kostele sv.
             Prokopa ve Žďáře nad Sázavou
           </li>
           <li className="border-l-4 pl-4 border-stone-800">
-            <div className="h-5 w-5 bg-stone-400 rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
+            <div className="h-5 w-5 bg-sunflower rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
             <storng className="font-semibold mr-4">14:00</storng> Společné focení
             před kostelem
           </li>
           <li className="border-l-4 pl-4 border-stone-800">
-            <div className="h-5 w-5 bg-stone-400 rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
+            <div className="h-5 w-5 bg-sunflower rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
             <storng className="font-semibold mr-4">15:15</storng> Oběd
           </li>
           <li className="border-l-4 pl-4 border-stone-800">
-            <div className="h-5 w-5 bg-stone-400 rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
+            <div className="h-5 w-5 bg-sunflower rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
             <storng className="font-semibold mr-4">16:15</storng> Krájení dortu
           </li>
           <li className="border-l-4 pl-4 border-stone-800 pb-4">
-            <div className="h-5 w-5 bg-stone-400 rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
+            <div className="h-5 w-5 bg-sunflower rounded-full  border-4 border-stone-800 relative top-6 right-7 "></div>
             <storng className="font-semibold mr-4">18:30</storng> První tanec +
             večerní raut
           </li>
