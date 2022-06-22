f4e85a046c1719f9ec56884c9f035eaa203a321b
petrkucerak <petr@khome.cz>
2022-06-22 13:53:04 +0200

    Improve responivity

--- components/dary-form.js
+++ components/dary-form.js
@@ -5,16 +5,16 @@ export default function DaryForm() {
   return (
     <section className="w-full min-h-[60vh] flex flex-col justify-center items-center">
       <Content>
-        <div className="flex w-full justify-around items-start lg:flex-row flex-col">
+        <div className="flex w-full justify-around items-start lg:flex-row flex-col my-8">
           <div className="mb-10 lg:mb-0">
-            <h2 className="text-6xl mb-2">Svatební dary</h2>
+            <h2 className="text-5xl mb-2">Svatební dary</h2>
             <p className="max-w-[1000px] w-[85vw] lg:w-[30vw] text-xl">
               Když tu svatbu už děláme, tak bychom na ní rádi alespoň trošku
               vydělali. Tak posílejte penízky. Hodně penízků.
             </p>
           </div>
           <div className="mt-10 lg:mt-0">
-            <h2 className="text-6xl mb-2">Potvrzení účasti</h2>
+            <h2 className="text-5xl mb-2">Potvrzení účasti</h2>
             <p className="max-w-[1000px] w-[85vw] lg:w-[30vw] text-xl">
               Máš hlad? To já taky. Tak se koukej přihlásti, jinak pro tebe
               nebude nic připraveného kulišáku!
--- components/kontakty.js
+++ components/kontakty.js
@@ -3,19 +3,19 @@ import Content from "../components/content";
 
 export default function Kontakty() {
   return (
-    <footer className="w-full min-h-[70vh] flex flex-col justify-center items-center bg-stone-900 text-white">
+    <footer className="w-full min-h-[70vh] flex flex-col justify-center items-center bg-stone-900 text-white py-8">
       <Content>
         <h2 className="text-6xl mb-5 text-center">Kontakty</h2>
-        <div className="text-2xl flex flex-col lg:flex-row justify-around items-start">
+        <div className="text-xl sm:text-2xl flex flex-col lg:flex-row justify-around items-start">
           <div className="flex justify-around items-center flex-row lg:flex-col mb-5 lg:mb-0">
             <img
               src="assets/image/ada.jpg"
-              className="rounded-full w-48 border-2 border-stone-800 mr-5 lg:mr-0"
+              className="rounded-full w-32 sm:w-48 border-2 border-stone-800 mr-5 lg:mr-0"
               alt="svedkyne Ada"
             ></img>
             <ul>
               <li>
-                <h3 className="font-mali text-3xl mt-4 font-semibold">Adéla Slámová</h3>
+                <h3 className="font-mali sm:text-3xl text-2xl mt-4 font-semibold">Adéla Slámová</h3>
               </li>
               <li>svědkyně</li>
               <li>
@@ -28,12 +28,12 @@ export default function Kontakty() {
           <div className="flex justify-around items-center flex-row lg:flex-col mt-5 lg:mt-0">
             <img
               src="assets/image/petr.jpg"
-              className="rounded-full w-48 border-2 border-stone-800 mr-5 lg:mr-0"
+              className="rounded-full w-32 sm:w-48 border-2 border-stone-800 mr-5 lg:mr-0"
               alt="Svedek Petr"
             ></img>
             <ul>
               <li>
-                <h3 className="font-mali text-3xl mt-4 font-semibold">Petr Kučera</h3>
+                <h3 className="font-mali sm:text-3xl text-2xl mt-4 font-semibold">Petr Kučera</h3>
               </li>
               <li>svědek</li>
               <li>
