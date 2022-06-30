d7c87a80e3eac6b86b251a766056bb3fdb2994d2
petrkucerak <petr@khome.cz>
2022-07-01 01:12:38 +0200

    Add stanovisetee

--- components/welcome.js
+++ components/welcome.js
@@ -1,7 +1,7 @@
 export default function Welcome() {
   return (
     <section className="flex w-full flex-col items-center justify-center min-h-screen">
-      <div className="w-[90vw] max-w-[1000px] flex flex-col items-center justify-center">
+      <div className="w-[90vw] max-w-[1000px] flex flex-col items-center justify-center my-8">
         <h2 className="text-4xl font-bold mb-4">Hlášení rozhlasu...</h2>
         <p className="mb-4">
           Prásk, buch, čvacht, … Už je to několik dnů, co se nad Českou
@@ -17,6 +17,18 @@ export default function Welcome() {
           které jsou ukryté kolem vás. Polohy surovin máte vyznačené na mapě.
           Abyste ji vždy získali, musíte správně odpovědět na danou otázku.
         </p>
+      </div>
+      <div className="w-[90vw] max-w-[1000px] flex flex-col items-center justify-center my-8">
+        <h2 className="text-4xl font-bold mb-4">Místa pro zisk surovin...</h2>
+        <ol className="list-decimal">
+          <li>Dřevo a trámy - 123456789, 123456789: ... ... ... ?</li>
+          <li>Igelity a jiné izolace</li>
+          <li>Zdravotnické potřeby</li>
+          <li>Elektrotechnika a nářadí</li>
+          <li>Jídlo a potraviny</li>
+          <li>Lůžkoviny a oblečení</li>
+          <li>Umělecké potřeby a knihy</li>
+        </ol>
       </div>
     </section>
   );
