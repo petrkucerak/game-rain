ea6ff16c185958bb0b914c382ad373003945fde8
petrkucerak <petr.kucera@mainstream.cz>
2022-06-30 22:26:03 +0200

    Remove IOs height improver, TODO: future

--- components/welcome.js
+++ components/welcome.js
@@ -8,24 +8,28 @@ export default function Welcome() {
     { url: "assets/image/welcome.png" },
   ];
   return (
-    <section id="welcome" className="w-full min-h-[90vh] flex flex-col justify-center items-center background-sun md:background-sun-fixed py-10 lg:py-0">
+    <section
+      id="welcome"
+      className="w-full min-h-[90vh] flex flex-col justify-center items-center background-sun md:background-sun-fixed py-10 lg:py-0"
+    >
       <Content>
         <h2 className="text-6xl mt-2 text-center">Něco o nás</h2>
         <div className="flex lg:flex-row flex-col justify-around my-5">
           <p className="w-full lg:mr-5 mt-5 text-xl">
-            Biliónech vascem karavel myši srovnatelné paliva jiní tj. spouští
-            měly přesněji lze projevy syndrom, výpravy{" "}
-            <strong>320denní poslechnout</strong> ho neonu. Mlze týmem, reliéfu,
-            výrazný, jehož, od u trpělivě geology o zajímavou háčků. Utká:
-            povodí poskytujících oddělující u války mzdu dáli. Vysoké bažin,
-            vidět tu tisíci odhalil zatím, pyramidy vějíř u těžko k vybrané ale
-            vládců náš dobyvačné tvrdě, divný nervových bažinatou na a nalezení.
-            Dospělého k čím i obličeje okem.
+            V jakém městě jsi vyrůstal? Kája: ve Žďáře nad Sázavou Filip: v
+            Hradci Králové Jaké máš koníčky? Kája: jízda na kole, skládání
+            puzzlí, čtení knížek, hudba Filip: hasičský sport, skaut, volejbal,
+            tanec Hraješ na něco? Kája: na klavír Filip: na kytaru Kde jste se
+            poprvé potkali? Poprvé jsme se potkali na spolču ve VKH v Hradci
+            Králové. Kdy jste zjistili, že spolu chodíte? Bylo to na podzim roku
+            2019. Stalo se to při přednášce Tomáše Halíka (neptejte se o čem
+            byla). Po skončení přednášky jsme se vzali za ruce. Od té chvíle
+            jsme spolu. Těšíme se na vás 😊
           </p>
-          <div
-            className="border-4 border-white rounded drop-shadow-2xl max-w-[1000px] w-[85vw] lg:mt-0 mt-5 h-full"
-          >
-            <img src={images[Math.floor(Math.random() * images.length) + 0].url}></img>
+          <div className="border-4 border-white rounded drop-shadow-2xl max-w-[1000px] w-[85vw] lg:mt-0 mt-5 h-full">
+            <img
+              src={images[Math.floor(Math.random() * images.length) + 0].url}
+            ></img>
           </div>
         </div>
       </Content>
