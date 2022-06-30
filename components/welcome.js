31fed3015d7fcfadc67379435b38100141c45e83
petrkucerak <petr.kucera@mainstream.cz>
2022-06-30 23:48:35 +0200

    sync

--- components/welcome.js
+++ components/welcome.js
@@ -15,17 +15,31 @@ export default function Welcome() {
       <Content>
         <h2 className="text-6xl mt-2 text-center">Něco o nás</h2>
         <div className="flex lg:flex-row flex-col justify-around my-5">
-          <p className="w-full lg:mr-5 mt-5 text-xl">
-            V jakém městě jsi vyrůstal? Kája: ve Žďáře nad Sázavou Filip: v
-            Hradci Králové Jaké máš koníčky? Kája: jízda na kole, skládání
-            puzzlí, čtení knížek, hudba Filip: hasičský sport, skaut, volejbal,
-            tanec Hraješ na něco? Kája: na klavír Filip: na kytaru Kde jste se
-            poprvé potkali? Poprvé jsme se potkali na spolču ve VKH v Hradci
-            Králové. Kdy jste zjistili, že spolu chodíte? Bylo to na podzim roku
-            2019. Stalo se to při přednášce Tomáše Halíka (neptejte se o čem
-            byla). Po skončení přednášky jsme se vzali za ruce. Od té chvíle
-            jsme spolu. Těšíme se na vás 😊
-          </p>
+          <div className="w-full lg:mr-5 mt-5 text-lg">
+            <strong>V jakém městě jsi vyrůstal?</strong>
+            <ul>
+              <li>Kája: ve Žďáře nad Sázavou</li>
+              <li>Filip: v Hradci Králové</li>
+            </ul>
+            <strong>Jaké máš koníčky?</strong>
+            <ul>
+              <li>Kája: jízda na kole, skládání puzzlí, čtení knížek, hudba</li>
+              <li>Filip: hasičský sport, skaut, volejbal, tanec</li>
+            </ul>
+            <strong>Hraješ na něco?</strong>
+            <ul>
+              <li>Kája: na klavír</li>
+              <li>Filip: na kytaru</li>
+            </ul>
+            <strong>Kde jste se poprvé potkali?</strong>
+            <p>Poprvé jsme se potkali na spolču ve VKH v Hradci Králové.</p>
+            <strong>Kdy jste zjistili, že spolu chodíte?</strong>
+            <p>
+              Bylo to na podzim roku 2019. Stalo se to při přednášce Tomáše
+              Halíka (neptejte se o čem byla). Po skončení přednášky jsme se
+              vzali za ruce. Od té chvíle jsme spolu.
+            </p>
+          </div>
           <div className="border-4 border-white rounded drop-shadow-2xl max-w-[1000px] w-[85vw] lg:mt-0 mt-5 h-full">
             <img
               src={images[Math.floor(Math.random() * images.length) + 0].url}
