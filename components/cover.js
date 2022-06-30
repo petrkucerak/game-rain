375c6a8a1401d3fca4bef82d209ff09e9d6432ea
petrkucerak <petr.kucera@mainstream.cz>
2022-06-30 16:18:18 +0200

    shadows remove

--- components/cover.js
+++ components/cover.js
@@ -4,8 +4,8 @@ import CountDown from "./count-down";
 
 export default function Cover() {
   return (
-    <header className="w-full flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-5xl md:text-7xl text-center z-10">
+    <header className="w-full flex flex-col justify-center items-center bg-white z-10 min-h-screen">
+      <h1 className="text-5xl md:text-7xl text-center z-20">
         Karolína & Filip
       </h1>
       <p className="text-2xl md:text-4xl z-10">10. září 2022</p>
