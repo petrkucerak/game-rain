5c1015a3be64c4b5a9de91709e162b4dcea407b8
petrkucerak <petr@khome.cz>
2022-07-01 10:40:02 +0200

    Jidloo

--- pages/points/jidlo-a-potraviny.js
+++ pages/points/jidlo-a-potraviny.js
@@ -9,10 +9,10 @@ export default function JidloAPotraviny() {
       <Point
         name={"Jídlo a potraviny"}
         describtion="Říká lidem popírány tkáň migrace situace přepisovací internetová služby, kosti ujít gladiátora mj. fázi méně ho ruské chvíli, dá nejvyšší švédskou době mrazy laně občanské veliký hnědavého z za mobilní, v se ne povinné nikde, tři příbuzenských u archeologa telefonovala. Nízké daří významem zkrátka potřebami. "
-        coords="50.0841581N, 14.4083333E"
-        question="Co fakt se to tak stalo?"
-        answare="kamarade"
-        odpoved="sdsds"
+        coords="50.0758742N, 14.4168725E"
+        question="Na zemi se píše, že ve 42 tu byla honička. Jaká kombinace barev zobrazuje tento rok?"
+        answare="černobílá"
+        odpoved="#adAFS5A*"
       />
       <Footer />
     </main>
